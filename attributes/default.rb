e12d96dcb4147b520ea4ff0dbb39811a6e101d8d
Joseph Glanville <jpg@jpg.id.au>
2014-12-18 13:43:38 +1100

    Fix bug in plugins

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,8 @@ default['elasticsearch']['user'] = 'elasticsearch'
 default['elasticsearch']['group'] = 'elasticsearch'
 default['elasticsearch']['home'] = '/usr/share/elasticsearch'
 
+default['elasticsearch']['plugins'] = {}
+
 default['elasticsearch']['memory'] = {
   max: 512,
   min: 512
