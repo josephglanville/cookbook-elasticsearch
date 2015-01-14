22b5b234a9a9b889afd7fe153515494d83f9b11d
Joseph Glanville <jpg@jpg.id.au>
2015-01-14 12:35:17 +1100

    Add logging.yml support

--- recipes/config.rb
+++ recipes/config.rb
@@ -3,3 +3,9 @@ file ::File.join(node['elasticsearch']['conf']['path']['conf'], 'elasticsearch.y
   group node['elasticsearch']['group']
   content node['elasticsearch']['conf'].to_hash.to_yaml
 end
+
+file ::File.join(node['elasticsearch']['conf']['path']['conf'], 'logging.yml') do
+  owner node['elasticsearch']['user']
+  group node['elasticsearch']['group']
+  content node['elasticsearch']['logging'].to_hash.to_yaml
+end
