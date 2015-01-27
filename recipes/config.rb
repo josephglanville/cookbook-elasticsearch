e5fb18bcc0a746d7b69212f3a9f7421c20d8ed10
Joseph Glanville <jpg@jpg.id.au>
2015-01-27 12:06:00 +1000

    Force conversion from ImmutableMash to Hash

--- recipes/config.rb
+++ recipes/config.rb
@@ -1,11 +1,11 @@
 file ::File.join(node['elasticsearch']['conf']['path']['conf'], 'elasticsearch.yml') do
   owner node['elasticsearch']['user']
   group node['elasticsearch']['group']
-  content node['elasticsearch']['conf'].to_hash.to_yaml
+  content JSON.parse(node['elasticsearch']['conf'].to_json).to_yaml
 end
 
 file ::File.join(node['elasticsearch']['conf']['path']['conf'], 'logging.yml') do
   owner node['elasticsearch']['user']
   group node['elasticsearch']['group']
-  content node['elasticsearch']['logging'].to_hash.to_yaml
+  content JSON.parse(node['elasticsearch']['logging'].to_json).to_yaml
 end
