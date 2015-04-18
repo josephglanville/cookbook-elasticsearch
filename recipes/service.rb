2aa719c58b1615c6de5e4538e0524f23d565554f
Joseph Glanville <jpg@jpg.id.au>
2015-04-18 13:31:26 +1000

    Use Upstart

--- recipes/service.rb
+++ recipes/service.rb
@@ -1,3 +1,12 @@
-service 'elasticsearch' do
-  action :enable
+# Replace SysV init with Upstart
+execute 'disable-elasticsearch' do
+  command 'update-rc.d elasticsearch remove'
+  action :nothing
 end
+
+file '/etc/init.d/elasticsearch' do
+  action :delete
+  notifies :run, 'execute[disable-elasticsearch]'
+end
+
+file '/etc/init/elasticsearch.comf'
